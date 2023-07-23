ac3fdee9b04371a77ed969b88da49171a3492abc
buiduc9918 <97076379+buiduc9918@users.noreply.github.com>
2023-07-23 09:15:40 +0700

    update 2.0

--- src/main/java/com/example/project/SecurityConfig.java
+++ src/main/java/com/example/project/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.example.project;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+// import org.springframework.security.config.annotation.web.builders.WebSecurity;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -20,20 +24,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .roles("USER");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/login").permitAll() // Allow access to login page without authentication
-            .anyRequest().authenticated()
-            .and()
+                .requestMatchers("/login").permitAll() // Allow access to login page without authentication
+                .requestMatchers("/dashboard").hasRole("USER") // Require "USER" role to access the dashboard
+                .requestMatchers("/admin").hasRole("ADMIN") // Require "ADMIN" role to access the admin page
+                .anyRequest().authenticated()
+                .and()
             .formLogin()
-            .loginPage("/login") // Specify the custom login page URL
-            .defaultSuccessUrl("/dashboard") // Redirect to the dashboard after successful login
-            .and()
+                .loginPage("/login") // Specify the custom login page URL
+                .defaultSuccessUrl("/dashboard") // Redirect to the dashboard after successful login
+                .and()
             .logout()
-            .logoutUrl("/logout") // Specify the custom logout URL
-            .logoutSuccessUrl("/login") // Redirect to the login page after successful logout
-            .invalidateHttpSession(true)
-            .deleteCookies("JSESSIONID");
+                .logoutUrl("/logout") // Specify the custom logout URL
+                .logoutSuccessUrl("/login") // Redirect to the login page after successful logout
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+            // http.headers(null).frameOptions().Origin();
+            return http.build();
     }
 }
